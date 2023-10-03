9760c357cb7303de9ccd5a9dd94a1e891ed0b7d0
amenir88 <ameni.rommene@esprit.tn>
2023-10-03 08:43:19 +0100

    Seéance 3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularProjectTwin1
+# AngularTwin1Project
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.9.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.9.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { SlideComponent } from './slide/slide.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    SlideComponent,
+    UserListComponent,
+    FooterComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
